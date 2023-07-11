8b2cf79d16fc7b12b2b69b7eb77a02284426c08d
MasterIceZ <64594596+MasterIceZ@users.noreply.github.com>
2023-07-11 18:07:33 +0700

    sails

--- Retired/OJUZ/ioi07_sails.cc
+++ Retired/OJUZ/ioi07_sails.cc
@@ -3,13 +3,13 @@
  * SCHOOL	: RYW
  * TASK		: Sails
  * ALGO		: Fenwick Tree
- * DATE		: 21 Jan 2023
+ * DATE		: 17 Jul 2023
  * */
 
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-#include <ext/rope>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+//#include <ext/rope>
 
 #ifndef _DEBUG
 // @==== Libary ====@ //
@@ -18,10 +18,10 @@
 #endif
 
 using namespace std;
-using namespace __gnu_pbds;
-using namespace __gnu_cxx;
+//using namespace __gnu_pbds;
+//using namespace __gnu_cxx;
 
-template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update> ;
+//template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update> ;
 
 // @=== Debugger ===@ //
 #ifdef _DEBUG
@@ -35,65 +35,82 @@ using ll = long long;
 
 const int MxN = 100010;
 
-struct fenwick_tree{
+struct fenwick_tree {
 	ll t[MxN];
-	void update(ll idx, ll v){
-		for(; idx<MxN; idx+=idx&-idx){
-			t[idx] = t[idx] + v;
+	inline void update(int idx, ll val) {
+		for(; idx<MxN; idx+=idx&=idx) {
+			t[idx] = t[idx] + val;
 		}
 	}
-	ll query(ll idx){
+	inline ll query(int idx) {
 		ll res = 0ll;
-		for(; idx>0; idx-=idx&-idx){
+		for(; idx>0; idx-=idx&-idx) {
 			res = res + t[idx];
 		}
 		return res;
 	}
-} fw;
+};
 
-pair<ll, ll> a[MxN];
+fenwick_tree fw;
+ll current_value;
+pair<ll, ll> p[MxN];
 
-ll get_idx(ll x){
-	int l = 0, r = MxN;
-	while(l < r){
-		int mid = (l + r + 1) >> 1;
-		dbg(l, mid, r);
-		if(fw.query(mid) >= x){
-			l = mid;
+inline ll tryLeft(ll l, ll r) {
+	ll res = l;
+	while(l < r) {
+		ll mid = (l + r) >> 1ll;
+		ll query_mid = fw.query(mid);
+		if(query_mid == current_value) {
+			r = mid;
+			res = mid;
 		}
-		else{
-			r = mid - 1;
+		else {
+			l = mid + 1ll;
 		}
 	}
-	return l;
+	return res;
+}
+
+inline ll tryRight(ll l, ll r) {
+	ll res = r;
+	while(l < r) {
+		ll mid = (l + r) >> 1ll;
+		ll query_mid = fw.query(mid);
+		if(query_mid == current_value) {
+			l = mid + 1ll;
+			res = l;
+		}
+		else {
+			r = mid;
+		}
+	}
+	return res;
 }
 
 inline void solution(){
 	int n;
 	cin >> n;
-	for(int i=1; i<=n; ++i){
-		cin >> a[i].first >> a[i].second;
+	for(int i=1; i<=n; ++i) {
+		cin >> p[i].first >> p[i].second;
 	}
-	sort(a + 1, a + n + 1);
-	for(int i=1; i<=n; ++i){
-		int k = a[i].first - a[i].second;
-		fw.update(a[i].first, 1);
-		if(k == 0){
-			continue;
-		}
-		ll l = min(a[i].first, get_idx(k));
-		ll r = get_idx(fw.query(k) + 1ll);
-		fw.update(l, -1ll);
-		fw.update(r, -1ll);
-		fw.update(r + a[i].second - (a[i].first - l), 1ll);
-		dbg(i, "FINISHED");
+	sort(p + 1, p + n + 1);
+	for(int i=1; i<=n; ++i) {
+		ll middle = p[i].first - p[i].second + 1ll;
+		current_value = fw.query(middle);
+		ll go_left = tryLeft(1ll, middle);	
+		ll go_right = tryRight(middle, p[i].first);
+		dbg(go_left, go_right);
+		fw.update(go_right + 1ll, 1ll);
+		fw.update(p[i].first + 1ll, -1ll);
+		fw.update(go_left, 1ll);
+		fw.update(go_left + p[i].second - (p[i].first - go_right), -1ll);
 	}
-	ll answer = 0ll;
-	for(int i=1; i<MxN; ++i){
-		ll now = fw.query(i);
-		answer += now * (now - 1) / 2;
+	ll res = 0ll;
+	for(int i=1; i<MxN; ++i) {
+		ll query_i = fw.query(i);
+		res = res + ((query_i * (query_i - 1ll)) >> 1ll);
 	}
-	cout << answer;
+	cout << res;
 	return ;
 }
 
